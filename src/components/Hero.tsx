c96640f1f9ad8dc1ede5dd865f9fe1d2f0723e6e
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-12 12:13:12 +0100

    Adjust Hero component layout and styling for improved responsiveness and readability

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,9 @@ import { Search } from "nextra/components"
 
 export const Hero = () => {
   return (
-    <section className="container w-[70cqw] grid lg:grid-cols-1 place-items-center pt-20 md:pt-20 pb-10 md:pb-10 gap-10">
+    <section className="container w-[90cqw] md:w-[70cqw] grid lg:grid-cols-1 place-items-center pt-20 md:pt-20 pb-10 md:pb-10 gap-10">
       <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
+        <main className="text-4xl md:text-6xl font-bold">
           <h2 className="text-[20px]/20 font-bold">OnePass Help Center</h2>
           <h1 className="inline">What can we help you find?</h1>
         </main>
@@ -12,9 +12,9 @@ export const Hero = () => {
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
           Find product docs, support articles, guides, and learning resources.
         </p>
-        <div className="space-y-4 md:space-y-0 md:space-x-4 flex z-2">
+        <div className="space-y-4 md:space-y-0 md:space-x-4 md:w-10/12 md:flex z-2">
           <Search
-            className="scale-150 origin-left"
+            className="md:scale-150 origin-left"
             placeholder="Search for a topic or question"
           />
         </div>
@@ -22,5 +22,5 @@ export const Hero = () => {
         <div className="space-y-4 md:space-y-0 md:space-x-4"></div>
       </div>
     </section>
-  )
+  );
 }
