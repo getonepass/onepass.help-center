1d7e634fbb910c78833f99c507ce0506b707e0dc
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-15 00:22:22 +0100

    Refactor API route for fetching posts: streamline code and improve readability

--- src/app/api/list/route.tsx
+++ src/app/api/list/route.tsx
@@ -1,17 +1,15 @@
-
 import type { NextRequest } from "next/server"
-import { normalizePages } from "nextra/normalize-pages";
-import { getPageMap } from "nextra/page-map";
+import { normalizePages } from "nextra/normalize-pages"
+import { getPageMap } from "nextra/page-map"
 
 interface ServiceProps {
-  name: string;
-  title: string;
-  route: string;
-  type: string;
-  frontMatter?: Record<string, any>;
+  name: string
+  title: string
+  route: string
+  type: string
+  frontMatter?: Record<string, any>
   // Add any other properties that you need
 }
-
 
 async function getPosts(path: string): Promise<ServiceProps[]> {
   const { directories } = normalizePages({
@@ -23,14 +21,12 @@ async function getPosts(path: string): Promise<ServiceProps[]> {
     .filter((post) => post.type !== "separator") as unknown as ServiceProps[]
 }
 
-
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const route = searchParams.get("route") || "";
-  const posts = await getPosts(route);
-
+  const searchParams = request.nextUrl.searchParams
+  const route = searchParams.get("route") || ""
+  const posts = await getPosts(route)
 
   return new Response(JSON.stringify(posts), {
     headers: { "content-type": "application/json" },
-  });
+  })
 }
