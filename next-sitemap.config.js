36860845b2aaa7405a7f805a9097a13e243c85eb
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-17 12:59:41 +0100

    Update next-sitemap.config.js formatting and subproject commit reference in guides

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
-	siteUrl: process.env.SITE_URL || "https://help.getonepass.eu",
-	generateRobotsTxt: true,
-	outDir: "out",
-	// ...other options
-};
+  siteUrl: process.env.SITE_URL || "https://help.getonepass.eu",
+  generateRobotsTxt: true,
+  outDir: "out",
+  // ...other options
+}
